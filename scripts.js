56faf34729442d123aa158f0f3d8c241f6cb5698
guilherme gouveia da costa <guigc0408@gmail.com>
2023-01-26 19:56:24 -0300

    versão com layout tela 2 pronto

--- scripts.js
+++ scripts.js
@@ -1,22 +1,29 @@
 const url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes';
 
-function getQuizzes () {
+function getQuizzes() {
     const quizzes = axios.get(`${url}`);
 
-    quizzes.then (res =>{
+    quizzes.then(res => {
         const quizzList = document.querySelector('.quizzes-part');
         quizzList.innerHTML = '';
         let quizzInfo = res.data;
-        quizzInfo.forEach( res => {
+        quizzInfo.forEach(res => {
             quizzList.innerHTML += `
             <div class="quizz" onclick="openId(this)">
             <img src="${res.image}">
             <div class="quizz-overlay"></div>
             <h2>${res.title}</h2>
         </div>`
-    })
+        })
     })
     quizzes.catch(err => console.log(err))
 }
-
+function getUnicQuizz() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1');
+    promise.then(teste);
+    function teste(quiz1) {
+        console.log(quiz1);
+    }
+}
+getUnicQuizz()
 getQuizzes();
